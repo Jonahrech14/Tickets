370165256cffa2da6568809ba43d452a2527ab29
Jonah Rechtenbach <Jonahrec@outlook.com>
2022-06-28 12:51:22 -0600

    parsed json and read the file giving the dates available

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// https://www.etix.com/ticket//online/timedEntrySearch.do?method=getMonthSummaryData&event_id=1002345&year=2022&month=3
-	result, err := http.Get("https://www.etix.com/ticket//online/timedEntrySearch.do?method=getMonthSummaryData&event_id=1002345&year=2022&month=3")
+	// https://www.etix.com/ticket//online/timedEntrySearch.do?method=getMonthSummaryData&event_id=1002345&year=2022&month=7
+	result, err := http.Get("https://www.etix.com/ticket//online/timedEntrySearch.do?method=getMonthSummaryData&event_id=1002345&year=2022&month=7")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	defer result.Body.Close()
+
+	dec := json.NewDecoder(result.Body)
+
+	m := map[string]interface{}{}
+	err = dec.Decode(&m)
 	if err != nil {
 		log.Fatal(err)
 	}
-	result.Body.Close()
+
+	for k := range m {
+		fmt.Println(k)
+		fmt.Println(m[k])
+	}
+	fmt.Println(m["A-#fafafa"])
 }
